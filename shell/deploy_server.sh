0c69f6d296fa6ea6f33da8337d6f4e8502fd4ec9
zekses <enmarantispam@gmail.com>
2020-11-20 22:45:24 +0300

    ....

--- shell/deploy_server.sh
+++ shell/deploy_server.sh
@@ -37,11 +37,22 @@ CopyFilesToFolder scripts Run/dbcode $dirname/$deployfolder/dbcode  ".sql"
 mkdir $dirname/$deployfolder/Logs
 mkdir $dirname/$deployfolder/ServerData
 
+export LD_LIBRARY_PATH=/usr/local/lib:$QT_BASE_DIR/lib
 #copying .so dependencies to libs folder
 for filename in "${executableFiles[@]}"
 do
 	./shell/cpld.sh $dirname/$deployfolder/$filename $dirname/$deployfolder/libs
 done
+	./shell/cpld.sh $dirname/$deployfolder/libs/libgrpc++.so.1 $dirname/$deployfolder/libs
+	./shell/cpld.sh $QT_BASE_DIR/lib/libQt5Charts.so $dirname/$deployfolder/libs
+echo 'iterating'
+for filename in $dirname/$deployfolder/libs/*; do
+	./shell/cpld.sh $filename $dirname/$deployfolder/libs
+done
+for filename in $QT_BASE_DIR/qml/QtQuick.2/*; do
+	./shell/cpld.sh $filename $dirname/$deployfolder/libs
+done
+
 
 
 strip -s -R .comment -R .gnu.version  $dirname/$deployfolder/feed_server
@@ -49,8 +60,9 @@ strip -s -R .comment -R .gnu.version  $dirname/$deployfolder/feed_server
 #copying qt dependencies
 echo "Operation: Copying Qt Dependencies"
 echo "plugins"
+    mkdir -p $deployfolder/sqldrivers
 cp -rf $QT_BASE_DIR/plugins $dirname/$deployfolder/libs/plugins
 cp -rf $QT_BASE_DIR/plugins $dirname/$deployfolder/plugins
-cp -rf $QT_BASE_DIR/plugins/sqldrivers $dirname/$deployfolder/sqldrivers
+cp -rf $QT_BASE_DIR/plugins/sqldrivers/libqsqlite.so $deployfolder/sqldrivers
 
-
+find $dirname/$deployfolder -name "*.so.debug" -type f -delete
