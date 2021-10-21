6aa1d6ed9bc79b0f1096570297b01767103f1ddf
zekses <enmarantispam@gmail.com>
2021-10-21 15:15:48 +0300

    modifying the flaresolverr request file

--- shell/flare_post.js
+++ shell/flare_post.js
@@ -1,9 +1,5 @@
-{
+'{
   "cmd": "request.get",
   "url":"%1",
-  "userAgent": "fanfiction recommendation engine(flipper) crawler",
-  "maxTimeout": 60000,
-  "headers": {
-    "X-Test": "fanfiction recommendation engine(flipper) crawler"
-  }
-}
+  "maxTimeout": 60000
+}'
